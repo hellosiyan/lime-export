2db7e4e86afb42bb08dfa6f02cf5f4680ac6cb73
Siyan Panayotov <xsisqox@gmail.com>
2011-10-05 23:45:29 +0300

    Minor js adjustments

--- assets/func.js
+++ assets/func.js
@@ -1,8 +1,8 @@
 jQuery(function($) {
 	var form = $('form#export-filters');
 
-	if ( form.length ) {(function(){
-		// Export Page
+	// Export Page
+	if ( form.length > 0 ) {(function(){
 		$('input:radio[name=wple_preset]', form).change(function() {
 			if ( $(this).val() == 'standard' ) {
 				$('.export-settings', form).slideUp();
@@ -10,19 +10,11 @@ jQuery(function($) {
 				$('.export-settings', form).slideDown();
 			}
 		}).filter(':checked').each(function() {
-			if ( $(this).val() == 'standard' ) {
-				$('.export-settings', form).hide();
-			} else {
-				$('.export-settings', form).show();
-			}
+			$('.export-settings', form).toggle( $(this).val() != 'standard' );
 		});
 
 		$('.tables-list li input:checkbox', form).change(function() {
-			if ( $(this).is(':checked') ) {
-				$(this).closest('li').removeClass('inactive');
-			} else {
-				$(this).closest('li').addClass('inactive');
-			}
+			$(this).closest('li').toggleClass('inactive', $(this).is(':not(:checked)'));
 		}).change();
 
 		$('a[data-action=select-all]', form).click(function() {
@@ -31,16 +23,16 @@ jQuery(function($) {
 		});
 
 		$('a[data-action^=select-]', form).click(function() {
+			var tables_list = $(this).closest('.tables-list');
 			switch ( $(this).data('Action') ) {
 				case 'select-all':
-					$(this).closest('.tables-list').find('input:checkbox:not(:checked)').click();
+					tables_list.find('input:checkbox:not(:checked)').click();
 					break;
 				case 'select-none':
-					$(this).closest('.tables-list').find('input:checkbox:checked').click();
+					tables_list.find('input:checkbox:checked').click();
 					break;
 				case 'select-standard':
-					$(this).closest('.tables-list').find('li.standard input:checkbox:not(:checked)').click();
-					$(this).closest('.tables-list').find('li:not(.standard) input:checkbox:checked').click();
+					tables_list.find('li.standard input:checkbox:not(:checked), li:not(.standard) input:checkbox:checked').click();
 					break;
 			}
 			
@@ -62,31 +54,35 @@ jQuery(function($) {
 				return false;
 			};
 
-			$('div.updated').css('overflow', 'hidden').animate({height: 0, opacity: 0}, 500,function() {
+			$('div.updated').css({'overflow': 'hidden'}).animate({height: 0, opacity: 0}, 500, function() {
 				$(this).remove();
 			});
 		});
 	})()};
 
 	// Snapshots page
-	$('.snapshot-description').each(function() {
-		var tables = $(this).find('ul:first');
-		if ( tables.length > 0 ) {
-			tables.hide().prev().append($('<a href="#">View all <small>(' + tables.children().length + ')</small></a>').click(function() {
-				if ( tables.is(':visible') ) {
-					tables.slideUp();
-					$(this).html('View all <small>(' + tables.children().length + ')</small>');
-				} else {
-					tables.slideDown();
-					$(this).text('Hide');	
-				}
-				return false;
-			}));
-		};
-	});
+	if ( $('.wple-snapshots').length > 0 ) {(function(){
+		$('.snapshot-description').each(function() {
+			var $th = $(this),
+				tables = $th.find('ul:first');
+			
+			if ( tables.length > 0 ) {
+				tables.hide().prev().append($('<a href="#">View all <small>(' + tables.children().length + ')</small></a>').click(function() {
+					if ( tables.is(':visible') ) {
+						tables.slideUp();
+						$th.html('View all <small>(' + tables.children().length + ')</small>');
+					} else {
+						tables.slideDown();
+						$th.text('Hide');	
+					}
+					return false;
+				}));
+			};
+		});
 
-	$('.snapshot-title .delete a').click(function() {
-		return confirm('DELETE snapshot ' + $(this).closest('.snapshot-title').find('strong:first').text() + '?');
-	});
+		$('.snapshot-title .delete a').click(function() {
+			return confirm('DELETE snapshot ' + $(this).closest('.snapshot-title').find('strong:first').text() + '?');
+		});
+	})();};
 
 });
